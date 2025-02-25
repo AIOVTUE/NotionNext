b5b10efb188c36ee2115a991fb944111aabfb71f
AIOVTUE <146168353+AIOVTUE@users.noreply.github.com>
2025-02-25 23:44:51 +0800

    Update config.js
    vdf

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -16,10 +16,10 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE: false
- // HEO_HERO_TITLE_1: '分享编程',
+
+//  HEO_HERO_TITLE_1: '分享编程',
 //  HEO_HERO_TITLE_2: '与思维认知',
-//  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
+ // HEO_HERO_TITLE_3: 'TANGLY1024.COM',
 //  HEO_HERO_TITLE_4: '新版上线',
 //  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
 //  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
@@ -37,14 +37,8 @@ const CONFIG = {
   //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
- // HEO_INFOCARD_GREETINGS: [
-    '你好！我是',
-    '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
-    '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
-    '🤖️ 数码科技爱好者',
-    '🧱 团队小组发动机'
+  HEO_INFOCARD_GREETINGS: [
+    '雨是神的烟花'
   ],
 
   // 个人资料底部按钮
